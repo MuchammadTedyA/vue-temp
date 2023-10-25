e249472760c8e22e03e8b546d168d8b1020b63e2
MuchammadTedyA <78480496+MuchammadTedyA@users.noreply.github.com>
2023-10-25 20:56:02 +0700

    start template

---
+++ src/components/forms/FormTag.vue
@@ -0,0 +1,35 @@
+<template>
+    <form 
+        @submit.prevent="submit"
+        :ref="name"
+        :event="event"
+        :action="action" 
+        :method="method" 
+        autocomplete="off" 
+        class="needs-validation" 
+        novalidate>
+        <slot></slot>
+    </form>
+</template>
+
+<script>
+export default {
+    name: 'FormTag',
+    props: ["action", "method", "name", "event"],
+    methods: {
+        submit() {
+            let myForm = this.$refs[this.$props.name];
+
+            if(myForm.checkValidity()) {
+                console.log("My event name", this.$props['event'])
+                console.log("Name", this.$props.name)
+                this.$emit(this.$props['event'])
+            }
+            
+            myForm.classList.add("was-validated")
+        }
+    },
+    
+    
+}
+</script>
